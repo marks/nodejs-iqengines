65abd004557291c0128190709548e85e85d871f6
huy <huy@huyng.com>
2011-06-02 22:37:44 -0700

    added nodeunit tests

--- tests.js
+++ tests.js
@@ -2,40 +2,57 @@
 var iqengines = require('./lib/iqengines.js');
 var assert    = require('assert');
 var device_id = (new Date()).toISOString();
-var api       =  new iqengines.Api();
+var api       = new iqengines.Api();
+var testCase  = require('nodeunit').testCase;
 
-assert.ok(process.env.IQE_SECRET, "no IQE_SECRET found in environment");
-assert.ok(process.env.IQE_KEY, "no IQE_KEY found in environment");
+module.exports.testEnvironmentSetup = testCase({
+    test: function (test) {
+        test.ok(process.env.IQE_SECRET, "no IQE_SECRET found in environment");
+        test.ok(process.env.IQE_KEY, "no IQE_KEY found in environment");
+        test.done();
+    }
+});
 
-console.log('=> Sending sample query ..');
-qid = api.sendQuery(
-    {
-        img: './testdata/default.jpg',
-        device_id:device_id
-    }, 
-    function (res) {
-        // console.log(res);
-        // console.log(qid);
-        // console.log(device_id);
-        console.log('=> Waiting for results ..');
-        assert.deepEqual(res, { data: { error: 0 } }, "unexpected data after querying");
-        api.waitResults({
-            device_id:device_id
-        },
-        function (res) {
-            
-            // console.log(JSON.stringify(res));
-            // console.log(res.data.results[0].qid);
-            console.log('=> Received results verifying ..');
-            assert.equal(res.data.results[0].qid, qid, "qid from waitResults does not match the qid sent");
-            console.log('=> Retrieving results manuall');
-            api.getResult({qid:qid}, function(res){
+module.exports.testQueryApi = testCase({
+    test: function (test) {
+        console.log('=> Sending sample query ..');
+        qid = api.sendQuery(
+            {
+                img: './testdata/default.jpg',
+                device_id:device_id
+            }, 
+            function (res) {
                 // console.log(res);
-            });
-        });
-        
-    });
+                // console.log(qid);
+                // console.log(device_id);
+                console.log('=> Waiting for results ..');
+                test.deepEqual(res, { data: { error: 0 } }, "unexpected data after querying");
+                api.waitResults({
+                    device_id:device_id
+                },
+                function (res) {
 
-console.log('=> Testing out simplerequest ..');
-sRequest = require('./lib/simplerequest.js');
-sRequest.test();
+                    // console.log(JSON.stringify(res));
+                    // console.log(res.data.results[0].qid);
+                    console.log('=> Received results verifying ..');
+                    test.equal(res.data.results[0].qid, qid, "qid from waitResults does not match the qid sent");
+                    console.log('=> Retrieving results manuall');
+                    api.getResult({qid:qid}, function(res){
+                        test.done();
+                        // console.log(res);
+                    });
+                });
+
+            });
+            
+    }
+});
+
+module.exports.testSimpleRequest = testCase({
+    test: function(test){
+        console.log('=> Testing out simplerequest ..');
+        sRequest = require('./lib/simplerequest.js');
+        sRequest.test();
+        test.done();
+    }
+});
